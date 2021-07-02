7a6bb97bd6c92f49fa6f491ab8f6c1830a5753fb
Max Mahlke <max.mahlke@oca.eu>
2021-07-02 11:27:51 +0200

    Adding examples and figure codes

---
+++ mcfa/examples.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python
+""" Simulation studies to illustrate MCFA."""
+
+import os
+
+# Run this example on the CPU only
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+from mcfa import MCFA
+
+
+def sample_3d(N):
+    """Draw points from two 3D Gaussian distributions."""
+
+    mu1 = np.array([0, 0, 0])
+    mu2 = np.array([2, 2, 6])
+
+    cov1 = np.array(
+        [
+            [4, -1.8, -1],
+            [-1.8, 2, 0.9],
+            [-1, 0.9, 2],
+        ]
+    )
+    cov2 = np.array(
+        [
+            [4, 1.8, 0.8],
+            [1.8, 2, 0.5],
+            [0.8, 0.5, 2],
+        ]
+    )
+    cluster1 = np.random.multivariate_normal(mu1, cov1, size=int(np.ceil(N / 2)))
+    cluster2 = np.random.multivariate_normal(mu2, cov2, size=int(np.floor(N / 2)))
+    data = np.append(cluster1, cluster2, axis=0)
+    return data
+
+
+def baek_2010_section_5(dropout=0.0):
+    """Fit the MCFA model to the example application in Baek+ 2010.
+
+    Parameters
+    ==========
+    dropout : float
+        Fraction of points to randomly replace by NaN. Default is 0.
+    """
+    seed = 37  # set seed for reproducibility
+    np.random.seed(seed)
+
+    epochs = 100  # number of training epochs
+
+    g = 2  # number of mixture components
+    q = 2  # number of latent dimensions
+
+    # ------
+    # Define the underlying multivariate Gaussian distributions
+    N = 500  # number of random samples drawn
+
+    data = sample_3d(N)
+
+    # Dropout data if specified - nice one-liner from
+    # https://stackoverflow.com/a/32182680/6162422
+    if dropout:
+        data.ravel()[
+            np.random.choice(data.size, int(data.size * dropout), replace=False)
+        ] = np.nan
+
+    # Some points may have had all observations removed - we resample those
+    if any(np.isnan(data).all(axis=1)):
+        data[np.isnan(data).all(axis=1)] = sample_3d(
+            len(data[np.isnan(data).all(axis=1)])
+        )
+    # ------
+    # Train the MCFA model
+    model = MCFA(n_components=g, n_factors=q)
+
+    # Fit the model parameters using ML
+    model.fit(
+        data,
+        n_epochs=500,
+        learning_rate=1e-4,
+        frac_validation=0.1,
+        converge_epochs=10,
+        batch_size=4,
+    )
+
+    # Compute the cluster probabilities of the observations
+    model.transform(data)
+
+    if dropout:
+        # Impute the missing data based on the observation's assigned clusters
+        model.impute()
+
+    # ------
+    # Plot the model properties
+    model.plot_data_space()
+    model.plot_latent_loadings()
+    model.plot_latent_space()
+    model.plot_loss()
+
+
+if __name__ == "__main__":
+
+    # Compute example of Baek+ 2010, section 5
+    baek_2010_section_5(dropout=0.0)
+
+    # Compute example of Baek+ 2010, section 5, randomly removing 30% of the observations
+    baek_2010_section_5(dropout=0.2)
---
+++ mcfa/figures.py
@@ -0,0 +1,282 @@
+#!/usr/bin/env python
+
+"""Functions to plot the data and latent space of the MCFA models."""
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+
+CLUSTER_COLORS = np.array(["steelblue", "firebrick"])
+
+
+def plot_data_space(model):
+    """Plot the model training data, colour-coded by cluster if the model has
+    been trained.
+
+    Parameters
+    ==========
+    model : core.MCFA
+        An MCFA model instance.
+
+    Notes
+    =====
+    Datapoints with at least one dimension imputed are displayed with half opacity.
+    """
+
+    # Create triangular figure of 2d projections of dimension pairs
+    fig, axes = plt.subplots(ncols=model.p - 1, nrows=model.p - 1, figsize=(16, 10))
+
+    # Get the cluter moments in data space
+    clusters_mean, clusters_cov, _, _ = model.compute_cluster_moments()
+    clusters_mean = clusters_mean.numpy()
+
+    for i in range(model.p - 1):
+        for j in range(model.p - 1):
+
+            ax = axes[i, j]
+            px = j  # index of data feature to plot on x-axis
+            py = i + 1  # index of data feature to plot on y-axis
+
+            if px >= py:
+                ax.axis("off")
+                continue
+
+            # Plot the training data
+            ax.scatter(
+                model.Y[:, px],
+                model.Y[:, py],
+                marker="o",
+                c=CLUSTER_COLORS[model.clusters],
+            )
+
+            # ------
+            # Add missing datapoints and true location if known
+            if hasattr(model, "Y_imp"):
+
+                ax.scatter(
+                    model.Y_imp[:, px],
+                    model.Y_imp[:, py],
+                    marker="o",
+                    alpha=0.5,
+                    c=CLUSTER_COLORS[model.clusters],
+                )
+
+                # Add the known values to the edges of the subplots
+                # by replacing the NaNs with lower limits
+                xmin, xmax = ax.get_xlim()
+                ymin, ymax = ax.get_ylim()
+
+                data_x_or_y_missing = np.isnan(model.Y[:, [px, py]]).any(axis=1)
+                data_xy = model.Y[:, [px, py]][data_x_or_y_missing]
+
+                data_xy[np.isnan(data_xy[:, 0]), 0] = xmin - 0.1
+                data_xy[np.isnan(data_xy[:, 1]), 1] = ymin - 0.1
+
+            # ------
+            # Add the modeled clusters
+            for k in range(model.n_components):
+
+                ax.scatter(
+                    clusters_mean[k, px],
+                    clusters_mean[k, py],
+                    color="black",
+                    marker="x",
+                )
+
+                confidence_ellipse(
+                    mean=clusters_mean[k][[px, py]],
+                    cov=clusters_cov[k][np.ix_([px, py], [px, py])],
+                    ax=ax,
+                    edgecolor="black",
+                    ls="--",
+                )
+
+            ax.set(xlabel=f"{px + 1}", ylabel=f"{py + 1}", title="Data Space")
+
+    plt.tight_layout()
+    plt.show()
+
+
+def plot_latent_space(model):
+    """Plot the model data and clusters in latent space.
+
+    Parameters
+    ==========
+    model : core.MCFA
+        An MCFA model instance.
+
+    Notes
+    =====
+    Datapoints with at least one dimension imputed are displayed with half opacity.
+    """
+
+    # Create triangular figure of 2d projections of dimension pairs
+    fig, axes = plt.subplots(
+        ncols=model.n_factors - 1, nrows=model.n_factors - 1, figsize=(12, 7.5)
+    )
+
+    # Compute the cluster moments and factor scores in latent spaces
+    _, _, clusters_mean, clusters_cov = model.compute_cluster_moments()
+    Z, Zmean, Zclust = model.compute_factor_scores()
+
+    for i in range(model.n_factors - 1):
+        for j in range(model.n_factors - 1):
+
+            if model.n_factors > 2:
+                ax = axes[i, j]
+            else:
+                ax = axes
+            dx = j  # index of data feature to plot on x-axis
+            dy = i + 1  # index of data feature to plot on y-axis
+
+            if dx >= dy:
+
+                ax.axis("off")
+                continue
+
+            for k in range(model.n_components):
+
+                idx_cluster = np.where(model.clusters == k)[0]
+
+                # Plot the factor scores by cluster
+                # Points with imputed dimensions are plotted with alpha=0.5
+                ax.scatter(
+                    Zmean[idx_cluster, dx],
+                    Zmean[idx_cluster, dy],
+                    marker="o",
+                    c=[CLUSTER_COLORS[k] for i in range(len(idx_cluster))],
+                    s=30,
+                    alpha=[
+                        0.5 if np.any(np.isnan(model.Y[ind])) else 1
+                        for ind in idx_cluster
+                    ],
+                )
+
+                # Add the cluster confidence ellipse
+                mean = clusters_mean[k][[dx, dy]]
+
+                ax.scatter(
+                    mean[0],
+                    mean[1],
+                    color="black",
+                    marker="x",
+                )
+
+                confidence_ellipse(
+                    mean=mean,
+                    cov=clusters_cov[k][np.ix_([dx, dy], [dx, dy])],
+                    ax=ax,
+                    edgecolor="black",
+                    ls="--",
+                )
+
+        ax.set(xlabel=f"{dx + 1}", ylabel=f"{dy + 1}", title="Latent space")
+
+    plt.tight_layout()
+    plt.show()
+
+
+def plot_latent_loadings(model, block=True):
+    """Plot the latent loadings of the trained model."""
+    fig, ax = plt.subplots(figsize=(16, 10))
+
+    A = model.W.numpy()
+    D, J = A.shape
+    xi = np.arange(D)
+
+    for j in range(J):
+        ax.plot(xi, A.T[j], "-", label=str(j + 1))
+
+    ax.axhline(0, ls=":", c="#000000", zorder=-1, lw=0.5)
+
+    ax.legend(frameon=False)
+    ax.set(xlim=(-0.5, D - 0.5), title="Latent Loadings")
+    ax.set_xticks(xi)
+
+    fig.tight_layout()
+    plt.show()
+
+
+def plot_loss(model):
+    """Plot the loss of the training and the validation sets over the model training."""
+
+    fig, ax = plt.subplots()
+
+    ax.plot(model.loss_training, ls="-", label="Training")
+    ax.plot(model.loss_validation, ls="-", label="Validation")
+
+    ax.legend(frameon=False)
+    ax.set(xlabel="Epochs", ylabel="Loss", title="Loss")
+
+    plt.tight_layout()
+    plt.show()
+
+
+# ------
+# Helper function from matplotliib
+def confidence_ellipse(mean, cov, ax, n_std=1.0, facecolor="none", **kwargs):
+    """
+    Create a plot of the covariance confidence ellipse of `x` and `y`
+
+    See how and why this works: https://carstenschelp.github.io/2018/09/14/Plot_Confidence_Ellipse_001.html
+
+    This function has made it into the matplotlib examples collection:
+    https://matplotlib.org/devdocs/gallery/statistics/confidence_ellipse.html#sphx-glr-gallery-statistics-confidence-ellipse-py
+
+    Or, once matplotlib 3.1 has been released:
+    https://matplotlib.org/gallery/index.html#statistics
+
+    I update this gist according to the version there, because thanks to the matplotlib community
+    the code has improved quite a bit.
+    Parameters
+    ----------
+    x, y : array_like, shape (n, )
+        Input data.
+    ax : matplotlib.axes.Axes
+        The axes object to draw the ellipse into.
+    n_std : float
+        The number of standard deviations to determine the ellipse's radiuses.
+    Returns
+    -------
+    matplotlib.patches.Ellipse
+    Other parameters
+    ----------------
+    kwargs : `~matplotlib.patches.Patch` properties
+    """
+    if len(mean) != 2:
+        print("mean must have length 2")
+    if cov.shape != (2, 2):
+        print("cov must be a 2x2 matrix")
+
+    pearson = cov[0, 1] / np.sqrt(cov[0, 0] * cov[1, 1])
+    # Using a special case to obtain the eigenvalues of this
+    # two-dimensionl dataset.
+    ell_radius_x = np.sqrt(1 + pearson)
+    ell_radius_y = np.sqrt(1 - pearson)
+    ellipse = mpl.patches.Ellipse(
+        (0, 0),
+        width=ell_radius_x * 2,
+        height=ell_radius_y * 2,
+        facecolor=facecolor,
+        **kwargs,
+    )
+
+    # Calculating the stdandard deviation of x from
+    # the squareroot of the variance and multiplying
+    # with the given number of standard deviations.
+    mean_x, mean_y = mean
+
+    scale_x = np.sqrt(cov[0, 0]) * n_std
+
+    # calculating the stdandard deviation of y ...
+    scale_y = np.sqrt(cov[1, 1]) * n_std
+
+    transf = (
+        mpl.transforms.Affine2D()
+        .rotate_deg(45)
+        .scale(scale_x, scale_y)
+        .translate(mean_x, mean_y)
+    )
+
+    ellipse.set_transform(transf + ax.transData)
+    return ax.add_patch(ellipse)
