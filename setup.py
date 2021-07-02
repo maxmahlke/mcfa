335991ad742b23d99d976ec4e8c3af5952229a04
Max Mahlke <max.mahlke@oca.eu>
2021-07-02 13:56:09 +0200

    Add setup script, update README

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+from setuptools import find_packages, setup
+
+setup(
+    name="mcfa",
+    version="0.1",
+    description="Mixtures of Common Factor Analyzers",
+    packages=find_packages(),
+    py_modules=["mcfa"],
+    install_requires=[
+        "tensorflow",
+        "matplotlib",
+        "numpy",
+        "tqdm",
+        "pandas",
+        "scipy",
+        "scikit-learn",
+    ],
+)
